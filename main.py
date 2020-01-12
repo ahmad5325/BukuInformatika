88b5c655c2367e585dc7c7625054628d3f724930
DindaMajesty <dindamesti@gmail.com>
2020-01-12 23:09:42 +0700

    if proyek or internship

--- main.py
+++ main.py
@@ -1,42 +1,60 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 21 20:54:32 2019
-
-@author: awangga
-"""
-
 import kepo
+import kepointernship
 import qrcode
-#import getpass
 
+print("Internship atau Proyek:")
+pilih=input()
+pilihan=pilih.lower()
 
-saya = kepo.Kepo()
-print("NPM : ")
-NPM=input()
-print("Proyek : ")
-PROYEK=input()
-print("Password SIAP : ")
-PASSWORD=input()
-print("Kode DOSEN : ")
-Pembimbing=input()
-print("Progres yang dilaporkan : ")
-Bimbingan=input()
-print("Nilai : ")
-Nilai=input()
-print("github.com/userrepo/namarepo/blob/master/namafile.pdf")
-print("userrepo : ")
-userrepo=input()
-print("namarepo : ")
-namarepo=input()
-print("namafile : ")
-namafile=input()
-print("pertemuan ke : ")
-pertemuan=input()
-
-
-crot=saya.generateURL(NPM, PASSWORD, PROYEK, Nilai, Bimbingan, Pembimbing,userrepo,namarepo,namafile,pertemuan)
-if crot != '':
-	img = qrcode.make(crot)
-	img.save("./"+NPM+".png")
-	print("File QrCode Telah Dibuat")
+if pilihan == "proyek":
+	saya = kepo.Kepo()
+	print("NPM : ")
+	NPM=input()
+	print("Proyek : ")
+	PROYEK=input()
+	print("Password SIAP : ")
+	PASSWORD=input()
+	print("Kode DOSEN : ")
+	Pembimbing=input()
+	print("Progres yang dilaporkan : ")
+	Bimbingan=input()
+	print("Nilai : ")
+	Nilai=input()
+	print("github.com/userrepo/namarepo/blob/master/namafile.pdf")
+	print("userrepo : ")
+	userrepo=input()
+	print("namarepo : ")
+	namarepo=input()
+	print("namafile : ")
+	namafile=input()
+	print("pertemuan ke : ")
+	pertemuan=input()
+	crot = saya.generateURL(NPM, PASSWORD, PROYEK, Nilai, Bimbingan, Pembimbing, userrepo, namarepo, namafile, pertemuan)
+	if crot != '':
+		img = qrcode.make(crot)
+		img.save("./" + NPM + ".png")
+		print("File QrCode Telah Dibuat")
+elif pilihan == "internship":
+	saya = kepointernship.Kepo()
+	print("NPM : ")
+	NPM = input()
+	print("Progres yang dilaporkan : ")
+	Bimbingan = input()
+	print("Nilai : ")
+	Nilai = input()
+	print("github.com/userrepo/namarepo/blob/master/namafile.pdf")
+	print("userrepo : ")
+	userrepo = input()
+	print("namarepo : ")
+	namarepo = input()
+	print("namafile : ")
+	namafile = input()
+	print("pertemuan ke : ")
+	pertemuan = input()
+	crot = saya.generateURL(NPM, Nilai, Bimbingan, userrepo, namarepo, namafile, pertemuan)
+	if crot != '':
+		img = qrcode.make(crot)
+		img.save("./" + NPM + ".png")
+		print("File QrCode Telah Dibuat")
+else:
+	print("eta kumaha typo....")
